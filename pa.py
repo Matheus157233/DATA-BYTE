12d3fc2fa6dbf5443a39e6cd70f1f3df456aa906
Matheus157233 <msantossaraiva69@gmail.com>
2026-04-01 16:10:05 -0300

    Update pa.py

--- pa.py
+++ pa.py
@@ -19,7 +19,7 @@ if "idioma" not in st.session_state:
 
 
 def aplicar_tema():
-    if st.session_state.tema == "Escuro":
+    if st.session_state.tema == "Escuro":
         st.markdown("""
         <style>
         body { background-color: #0e1117; color: white; }
